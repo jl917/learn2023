d1a1eea105758568504136bd3de7ba227b60ed6b
julong <julong1988@naver.com>
2022-12-21 18:35:17 +0900

    three

---
+++ t3ejs/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig(({ mode, command }) => {
+  return defineConfig({
+    root: './src',
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    plugins: [],
+  })
+});
