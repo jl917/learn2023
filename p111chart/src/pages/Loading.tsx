cad918c6da2a80b5f314476a6aee2fba4586d03e
julong <julong1988@naver.com>
2023-07-31 11:45:24 +0900

    loading

---
+++ p111chart/src/pages/Loading.tsx
@@ -0,0 +1,140 @@
+import { ECOption } from '@/types/echart';
+import * as echarts from 'echarts';
+import { useEffect, useRef } from 'react';
+import { debounce } from 'lodash-es'
+
+function randomNum(m: number, n: number): number {
+  return Math.floor(Math.random() * (m - n) + n)
+}
+
+const option: ECOption = {
+  // title: {
+  //   text: 'Stacked Area Chart'
+  // },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+      label: {
+        backgroundColor: '#6a7985'
+      }
+    }
+  },
+  legend: {
+    data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {}
+    }
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  xAxis: [
+    {
+      type: 'category',
+      boundaryGap: false,
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    }
+  ],
+  yAxis: [
+    {
+      type: 'value'
+    }
+  ],
+  series: [
+    {
+      name: 'Email',
+      type: 'line',
+      stack: 'Total',
+      areaStyle: {},
+      emphasis: {
+        focus: 'series'
+      },
+      data: [120, 132, 101, 134, 90, 230, 210]
+    },
+    {
+      name: 'Union Ads',
+      type: 'line',
+      stack: 'Total',
+      areaStyle: {},
+      emphasis: {
+        focus: 'series'
+      },
+      data: [220, 182, 191, 234, 290, 330, 310]
+    },
+    {
+      name: 'Video Ads',
+      type: 'line',
+      stack: 'Total',
+      areaStyle: {},
+      emphasis: {
+        focus: 'series'
+      },
+      data: [150, 232, 201, 154, 190, 330, 410]
+    },
+    {
+      name: 'Direct',
+      type: 'line',
+      stack: 'Total',
+      areaStyle: {},
+      emphasis: {
+        focus: 'series'
+      },
+      data: [320, 332, 301, 334, 390, 330, 320]
+    },
+    {
+      name: 'Search Engine',
+      type: 'line',
+      stack: 'Total',
+      label: {
+        show: true,
+        position: 'top'
+      },
+      areaStyle: {},
+      emphasis: {
+        focus: 'series'
+      },
+      data: [820, 932, 901, 934, 1290, 1330, 1320]
+    }
+  ],
+};
+
+
+
+const LoadingPage = () => {
+  const chartRef = useRef<echarts.EChartsType | null>(null);
+  const divRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+
+    if (divRef.current) {
+      chartRef.current = echarts.init(divRef.current);
+      chartRef.current.showLoading();
+      setTimeout(() => {
+        chartRef.current?.setOption(option);
+        chartRef.current?.hideLoading();
+      }, 2000)
+    }
+
+  }, []);
+
+  const onClick = () => {
+    const newValues = JSON.parse(JSON.stringify(option));
+    newValues.series[0].data[0] = randomNum(100, 500);
+    chartRef.current?.setOption(newValues);
+  }
+
+  return (
+    <>
+      <div className="chart" style={{ height: 500 }} ref={divRef} ></div>
+      <button onClick={onClick}>change</button>
+    </>
+  );
+};
+
+export default LoadingPage;
--- p111chart/src/router/index.tsx
+++ p111chart/src/router/index.tsx
@@ -4,6 +4,7 @@ import MainPage from '@/pages/Main';
 import DatasetPage from '@/pages/Dataset';
 import AxisPage from '@/pages/Axis';
 import EventPage from '@/pages/Event';
+import LoadingPage from '@/pages/Loading';
 
 const Router = () => {
   return (
@@ -13,6 +14,7 @@ const Router = () => {
         <Route path="/dataset" element={<DatasetPage />} />
         <Route path="/axis" element={<AxisPage />} />
         <Route path="/event" element={<EventPage />} />
+        <Route path="/loading" element={<LoadingPage />} />
       </Routes>
     </BrowserRouter>
   );
