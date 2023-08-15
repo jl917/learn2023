227ee7a6e91be08e118c4f57ce8d2ffdc397e26b
julong <julong1988@naver.com>
2023-08-15 17:59:37 +0900

    데이터 묶기

--- mongo/aggregate.js
+++ mongo/aggregate.js
@@ -1,7 +1,9 @@
+const dayjs = require("dayjs");
 const mongoose = require("mongoose");
+
 mongoose.connect("mongodb://127.0.0.1:27017/daoDev");
 
-const Log = mongoose.model("Log2", {
+const Log = mongoose.model("Log", {
   timestamp: Date,
   user: String,
   page: String,
@@ -9,16 +11,57 @@ const Log = mongoose.model("Log2", {
 
 const find = () => {
   const st = new Date().getTime();
+  // // 이름별 카운팅 하기.
+  // Log.aggregate([
+  //   {
+  //     $match: {
+  //       timestamp: { $gte: dayjs("2022-01").startOf("month").toDate(), $lt: dayjs("2022-03").endOf("month").toDate() },
+  //     },
+  //   },
+  //   { $limit: 10000 },
+  //   {
+  //     $group: { _id: "$user", total: { $sum: 1 } },
+  //   },
+  // ]).then((res) => {
+  //   console.log(res);
+  //   const et = new Date().getTime();
+  //   console.log(`수행시간: ${et - st}`);
+  //   // console.log(res);
+  // });
+
+  // 월별 카운팅 하기.
   Log.aggregate([
     {
       $match: {
-        timestamp: { $gte: new Date(2022, 1, 1), $lt: new Date(2022, 2, 1) },
+        timestamp: {
+          $gte: dayjs("2022-01").startOf("month").toDate(),
+          $lt: dayjs("2022-03").endOf("month").toDate(),
+        },
       },
     },
-    { $limit: 1000 },
+    { $limit: 10000 },
     {
-      $group: { _id: "$user", total: { $sum: 1 } },
+      $group: {
+        _id: {
+          year: { $year: "$timestamp" },
+          month: { $month: "$timestamp" },
+        },
+        total: { $sum: 1 },
+      },
     },
+    {
+      $project: {
+        _id: 0,
+        date: {
+          $concat: [
+            {$toString: "$_id.year"},
+            '-',
+            {$toString: "$_id.month"},
+          ]
+        },
+        total: 1,
+      }
+    }
   ]).then((res) => {
     console.log(res);
     const et = new Date().getTime();
